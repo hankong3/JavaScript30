e5877224d52d9038feef12ee6979f1ffcd86c17c
hankong <binnnkk@naver.com>
2019-05-20 16:51:06 +0900

    add display end time function

--- 29-Countdown_Timer/scripts.js
+++ 29-Countdown_Timer/scripts.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
+    displayEndTime(then);
 
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -24,4 +26,14 @@ function displayTimeLeft(seconds) {
     const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
     document.title = display;
     timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+    // Date.now() -> 1558338138099
+    // new Date(1558338138099) -> Mon May 20 2019 16:42:18 GMT+0900 (한국 표준시)
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
