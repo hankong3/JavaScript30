34c616bd21bb1bdb8601fd03879e7786fa77edbc
hankong <binnnkk@naver.com>
2019-05-29 15:04:14 +0900

    dataset variable

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -2,6 +2,7 @@ const inputs = document.querySelectorAll('.controls input');
 
 function changeImg() {
     console.log(this);
+    const pixel = this.dataset.sizing || '';
 }
 
 inputs.forEach(input => input.addEventListener('change', changeImg));
