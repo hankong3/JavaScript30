e644e24497cfda388420bc3cc1347fae8bed76fa
hankong <binnnkk@naver.com>
2019-06-03 16:40:09 +0900

    change line width

--- 08 - Fun with HTML5 Canvas/script.js
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -12,25 +12,38 @@ let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let color = 0;
+let line = true;
 
 function draw(e) {
     if (!isDrawing) return;
-    // console.log(e);
-    ctx.strokeStyle = `hsl(${color}, 100%, 50%)`;
-    ctx.beginPath();
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
-    [lastX, lastY] = [e.offsetX, e.offsetY];
 
-    color++;
-    if (color >= 360) color = 0;
+    // console.log(e);
+    ctx.strokeStyle = `hsl(${color++}, 100%, 50%)`;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);   // 선이 시작될 좌표를 설정
+    ctx.lineTo(e.offsetX, e.offsetY);   // 선이 끝나는 좌표를 설정
+    ctx.stroke();   // 선 그리기 시작
+    [lastX, lastY] = [e.offsetX, e.offsetY];    // 선이 시작될 좌표 업데이트
+
+    if (color >= 360) {
+        color = 0;
+    }
+
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+        line = !line;
+    }
+
+    if (line) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
 }
 
 if (canvas) {
     canvas.addEventListener('mousedown', (e) => {
         isDrawing = true;
-        [lastX, lastY] = [e.offsetX, e.offsetY];
+        [lastX, lastY] = [e.offsetX, e.offsetY];    // 선이 시작될 좌표 초기화
     });
     canvas.addEventListener('mousemove', draw);
     canvas.addEventListener('mouseup', () => isDrawing = false);
