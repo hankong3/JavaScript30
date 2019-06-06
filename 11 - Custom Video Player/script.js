f38daa51120e3f34bcf3564aec2da3598925b20a
hankong <binnnkk@naver.com>
2019-06-06 14:33:15 +0900

    get elements

---
+++ 11 - Custom Video Player/script.js
@@ -0,0 +1,7 @@
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const toggle = document.querySelector('.toggle');
+const ranges = document.querySelectorAll('.player__slider');
+const skipButtons = document.querySelectorAll('[data-skip]');
