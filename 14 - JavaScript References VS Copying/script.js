f17b7aeca34834fdadc292d02a79deb4cf900ba8
hankong <binnnkk@naver.com>
2019-06-09 15:10:15 +0900

    Array.prototype.slice()

--- 14 - JavaScript References VS Copying/script.js
+++ 14 - JavaScript References VS Copying/script.js
@@ -1,30 +1,30 @@
 // start with strings, numbers and booleans
-let name = 'Mario';
-console.log(name);
-name = 'Yoshi'
-console.log(name);
+// let name = 'Mario';
+// console.log(name);
+// name = 'Yoshi'
+// console.log(name);
 
-let num = 100;
-let num2 = 200;
-console.log(num, num2);
-num2 = 300;
-console.log(num, num2);
+// let num = 100;
+// let num2 = 200;
+// console.log(num, num2);
+// num2 = 300;
+// console.log(num, num2);
 
-let bool = false;
-console.log(bool);
-bool = true;
-console.log(bool);
+// let bool = false;
+// console.log(bool);
+// bool = true;
+// console.log(bool);
 
 // Let's say we have an array
 const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 
 // and we want to make a copy of it.
-const members = players;
-console.log(players, members);
+// const members = players;
+// console.log(players, members);
 
 // You might think we can just do something like this (ex: const team = players; team[3] = 'Yoshi';):
-members[3] = 'Yoshi';
-console.log(players, members);
+// members[3] = 'Yoshi';
+// console.log(players, members);
 
 // however what happens when we update that array?
 
@@ -35,6 +35,9 @@ console.log(players, members);
 // Why? It's because that is an array reference, not an array copy. They both point to the same array!
 
 // So, how do we fix this? We take a copy instead!
+const members2 = players.slice();
+members2[2] = 'Mario';
+console.log(players, members2);
 
 // one way
 
