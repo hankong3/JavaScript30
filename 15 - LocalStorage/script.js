1569906e0c83b738809591d26d497d4758e32c4b
hankong <binnnkk@naver.com>
2019-06-10 11:10:04 +0900

    toggle checkbox event

--- 15 - LocalStorage/script.js
+++ 15 - LocalStorage/script.js
@@ -20,11 +20,23 @@ function populateList(plates = [], platesList) {
     platesList.innerHTML = plates.map((plate, i) => {
         return `
             <li>
-                <input type="checkbox" data-index=${i} id="item${i}"}>
+                <input type="checkbox" data-index=${i} id="item${i}">
                 <label for="item${i}">${plate.text}</label>
             </li>
         `;
     }).join('');
 }
 
+function toggleDone(e) {
+    if (!e.target.matches('input')) return;
+    const target = e.target;
+    const index = target.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+}
+
 addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+
+populateList(items, itemsList);
