390aa1c513898c20b8902bbc63915cb1275e3fd5
hankong <binnnkk@naver.com>
2019-06-10 10:51:02 +0900

    add item list (innerHTML & map())

--- 15 - LocalStorage/script.js
+++ 15 - LocalStorage/script.js
@@ -11,8 +11,20 @@ function addItem(e) {
     }
 
     items.push(item);
+    populateList(items, itemsList);
     localStorage.setItem('items', JSON.stringify(items));
     this.reset();
+}
+
+function populateList(plates = [], platesList) {
+    platesList.innerHTML = plates.map((plate, i) => {
+        return `
+            <li>
+                <input type="checkbox" data-index=${i} id="item${i}"}>
+                <label for="item${i}">${plate.text}</label>
+            </li>
+        `;
+    }).join('');
 }
 
 addItems.addEventListener('submit', addItem);
