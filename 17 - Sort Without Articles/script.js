eb61d6416e9778b93b9792104f56babb79e23028
hankong <binnnkk@naver.com>
2019-06-12 16:47:05 +0900

    innerHTML & Array.prototype.map()

--- 17 - Sort Without Articles/script.js
+++ 17 - Sort Without Articles/script.js
@@ -9,3 +9,8 @@ function strip(band) {
 const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
 console.log(sortedBands);
+
+document.querySelector('#bands').innerHTML =
+    sortedBands
+        .map(band => `<li>${band}</li>`)
+        .join('');
