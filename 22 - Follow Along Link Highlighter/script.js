ce0abd51aecbebee62ed73797fa1447d7662e4cc
hankong <binnnkk@naver.com>
2019-06-17 14:01:41 +0900

    add coords & style property

--- 22 - Follow Along Link Highlighter/script.js
+++ 22 - Follow Along Link Highlighter/script.js
@@ -4,9 +4,18 @@ const highlight = document.createElement('span');
 highlight.classList.add('highlight');
 document.body.appendChild(highlight);
 
-function highlightLink() {
+function highlightLink(e) {
     const linkCoords = this.getBoundingClientRect();
-    console.log(linkCoords);
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX
+    }
+
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 }
 
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', highlightLink));
