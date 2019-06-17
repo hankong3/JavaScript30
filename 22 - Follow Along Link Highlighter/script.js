4dec168b1dc78e47d96abb0d6632c5ba5607b320
hankong <binnnkk@naver.com>
2019-06-17 13:47:57 +0900

    mouseenter event & getBoundingClientRect()

--- 22 - Follow Along Link Highlighter/script.js
+++ 22 - Follow Along Link Highlighter/script.js
@@ -3,3 +3,10 @@ const triggers = document.querySelectorAll('a');
 const highlight = document.createElement('span');
 highlight.classList.add('highlight');
 document.body.appendChild(highlight);
+
+function highlightLink() {
+    const linkCoords = this.getBoundingClientRect();
+    console.log(linkCoords);
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', highlightLink));
