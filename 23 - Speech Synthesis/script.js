290a1413c44c89e1c3e39b3660bbbb7818235923
hankong <binnnkk@naver.com>
2019-06-18 10:54:10 +0900

    speechSynthesis voiceschanged event

--- 23 - Speech Synthesis/script.js
+++ 23 - Speech Synthesis/script.js
@@ -4,3 +4,13 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+
+function populateVoices() {
+    voices = this.getVoices();
+    voicesDropdown.innerHTML = voices
+        .filter(voice => voice.lang.includes('en'))
+        .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+        .join('');
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
