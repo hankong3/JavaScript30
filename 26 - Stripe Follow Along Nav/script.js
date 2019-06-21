2cb3f696fc08e0f3e21513cbb22c8b28c01a1e53
hankong <binnnkk@naver.com>
2019-06-21 15:36:08 +0900

    moue enter event

---
+++ 26 - Stripe Follow Along Nav/script.js
@@ -0,0 +1,11 @@
+const triggers = document.querySelectorAll('.cool > li');
+const background  = document.querySelector('.dropdownBackground');
+const nav  = document.querySelector('.top');
+
+function  handleEnter() {
+    this.classList.add('trigger-enter');
+    setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+    background.classList.add('open');
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
