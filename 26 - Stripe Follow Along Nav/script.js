a92ecdaf5bcecf84a4ddd7e8273d4594be9324d9
hankong <binnnkk@naver.com>
2019-06-21 15:36:23 +0900

    mouse leave event

--- 26 - Stripe Follow Along Nav/script.js
+++ 26 - Stripe Follow Along Nav/script.js
@@ -8,4 +8,10 @@ function  handleEnter() {
     background.classList.add('open');
 }
 
+function handleLeave() {
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
+    background.classList.remove('open');
+  }
+
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
