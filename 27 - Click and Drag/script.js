6544b43488a6e1a1aac3d4a5a2fa629b766668bb
hankong <binnnkk@naver.com>
2019-06-22 18:12:55 +0900

    mouse move event

--- 27 - Click and Drag/script.js
+++ 27 - Click and Drag/script.js
@@ -17,3 +17,10 @@ slider.addEventListener('mouseleave', () => {
     isDown = false;
     slider.classList.remove('active');
 });
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3;
+    slider.scrollLeft = scrollLeft - walk;
+});
