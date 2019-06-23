076b0819ab624e3a559d8e0cd803a8da09487c8d
hankong <binnnkk@naver.com>
2019-06-23 15:09:51 +0900

    add mouse event (down, up, leave)

--- 27 - Click and Drag/script.js
+++ 27 - Click and Drag/script.js
@@ -1,2 +1,15 @@
 const slider = document.querySelector('.items');
 let isClick = false;
+
+slider.addEventListener('mousedown', () => {
+    isClick = true;
+    slider.classList.add('active');
+});
+slider.addEventListener('mouseup', () => {
+    isClick = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mouseleave', () => {
+    isClick = false;
+    slider.classList.remove('active');
+});
