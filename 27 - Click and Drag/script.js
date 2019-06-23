f11fccd34678ead784809d44076b489b77715052
hankong <binnnkk@naver.com>
2019-06-23 15:37:14 +0900

    add coordinates flag

--- 27 - Click and Drag/script.js
+++ 27 - Click and Drag/script.js
@@ -1,14 +1,18 @@
 const slider = document.querySelector('.items');
 let isClick = false;
+let startX;
+let scrollLeft;
 
 function stopSlider() {
     isClick = false;
     slider.classList.remove('active');
 }
 
-slider.addEventListener('mousedown', () => {
+slider.addEventListener('mousedown', (e) => {
     isClick = true;
     slider.classList.add('active');
+    startX = e.pageX;
+    scrollLeft = slider.scrollLeft;
 });
 slider.addEventListener('mouseup', stopSlider);
 slider.addEventListener('mouseleave', stopSlider);
