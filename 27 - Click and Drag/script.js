f1f66728e7d784a7ea6e6ebdf1351d9de6d57c30
hankong <binnnkk@naver.com>
2019-06-23 15:35:43 +0900

    define mouse event function (up, leave)

--- 27 - Click and Drag/script.js
+++ 27 - Click and Drag/script.js
@@ -1,15 +1,14 @@
 const slider = document.querySelector('.items');
 let isClick = false;
 
+function stopSlider() {
+    isClick = false;
+    slider.classList.remove('active');
+}
+
 slider.addEventListener('mousedown', () => {
     isClick = true;
     slider.classList.add('active');
 });
-slider.addEventListener('mouseup', () => {
-    isClick = false;
-    slider.classList.remove('active');
-});
-slider.addEventListener('mouseleave', () => {
-    isClick = false;
-    slider.classList.remove('active');
-});
+slider.addEventListener('mouseup', stopSlider);
+slider.addEventListener('mouseleave', stopSlider);
