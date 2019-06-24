8520534bdaa0a200203f1fd9b95460a4907c09ab
hankong <binnnkk@naver.com>
2019-06-24 21:20:36 +0900

    add start timer function

--- 29 - Countdown Timer/script.js
+++ 29 - Countdown Timer/script.js
@@ -1,6 +1,7 @@
 let countdown;
 const timeLeft = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
     const now = Date.now();
@@ -36,3 +37,10 @@ function displayEndTime(timestamp) {
 
     endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
