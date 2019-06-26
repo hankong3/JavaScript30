55e2ae3f0e333e72f0dc49aaf3e0da4a27158854
hankong <binnnkk@naver.com>
2019-06-26 15:38:38 +0900

    add random hole function

--- 30 - Whack A Mole/script.js
+++ 30 - Whack A Mole/script.js
@@ -1,7 +1,21 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
 
 function randTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
+}
+
+function randHole(holes) {
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+
+    if (hole === lastHole) {
+        console.log('Ah nah thats the same one bud');
+        return randHole(holes);
+    }
+
+    lastHole = hole;
+    return hole;
 }
